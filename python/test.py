7bc6e8ae1ba43add9edd467e2d626a31a488d319
EC2 Default User <ec2-user@ip-172-31-3-36.ap-southeast-2.compute.internal>
2016-02-14 12:02:09 +0000

    Added results

--- python/test.py
+++ python/test.py
@@ -2,7 +2,12 @@ import boto3
 import time
 import timeit
 
+#ARGHGHGHGHG!!!! 
+#Python3 is hanging when I redirect stdout! FML
+# SHitty Workaround!!
+
 def main(): 
+    results=""
     client = boto3.client('dynamodb', region_name='ap-southeast-2')
 
     def getRecord(): 
@@ -19,8 +24,14 @@ def main():
         )
         return response
 
-    while True: 
-        print("Milliseconds:", (timeit.timeit(getRecord, number=1) * 1000))
+    for x in range(0, 150): 
+        result = (timeit.timeit(getRecord, number=1) * 1000)
+        print(result)
+        results += str(result) + "\n"
         time.sleep(1)
+    
+    text_file = open("output.log", "w")
+    text_file.write(results)
+    text_file.close()
 
 main()
