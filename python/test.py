a9fda0e9373fb09cc056004e103a92e6c7b96466
EC2 Default User <ec2-user@ip-172-31-3-36.ap-southeast-2.compute.internal>
2016-02-14 11:06:41 +0000

    Corrected a number of runtime stuff on ec2

--- python/test.py
+++ python/test.py
@@ -2,22 +2,25 @@ import boto3
 import time
 import timeit
 
-client = boto3.client('dynamodb')
+def main(): 
+    client = boto3.client('dynamodb', region_name='ap-southeast-2')
 
-def getRecord(): 
-    table="test"
-    key="FOO"
-    response = client.get_item(
-        TableName=table,
-        ConsistentRead=False,
-        Key={
-            'key': {
-                'S': key,
+    def getRecord(): 
+        table="test"
+        key="FOO"
+        response = client.get_item(
+            TableName=table,
+            ConsistentRead=False,
+            Key={
+                'key': {
+                    'S': key,
+                }
             }
-        }
-    )
-    return response
+        )
+        return response
 
-while True: 
-    print("Milliseconds:", (timeit.timeit(getRecord, number=1) * 1000))
-    time.sleep(1)
+    while True: 
+        print("Milliseconds:", (timeit.timeit(getRecord, number=1) * 1000))
+        time.sleep(1)
+
+main()
